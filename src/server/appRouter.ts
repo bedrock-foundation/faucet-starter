6625413a2c9c027efc621b61e8948f442af8fd0d
Sam Heutmaker <samheutmaker@gmail.com>
2022-12-01 09:19:58 -0700

    remove uneeded changes

--- src/server/appRouter.ts
+++ src/server/appRouter.ts
@@ -1,18 +1,15 @@
 import { router } from './trpc';
 import scanService from './services/scan/scan.service';
 import faucetService from './services/faucet/faucet.service';
-import taskService from './services/task/task.service';
 
 export const appRouter = router({
   faucet: faucetService.router,
   scan: scanService.router,
-  task: taskService.router,
 });
 
 export const appCaller = appRouter.createCaller({});
 
 faucetService.setAppCaller(appCaller);
 scanService.setAppCaller(appCaller);
-taskService.setAppCaller(appCaller);
 
 export type AppRouter = typeof appRouter;
